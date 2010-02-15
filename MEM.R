a51234d541918341cc9cbd35a6a1d32a7b5518b6
MP.Etienne <MP.Etienne@3ae06b9c-c8ca-11de-9d4f-f947ee5921c8>
2010-02-15 23:40:46 +0000

    All estimators have been cleaned up, implementation of MEM1 + Mem2 + AIC TODO : CI through profile likelihood.

--- MEM.R
+++ MEM.R
@@ -4,148 +4,186 @@
 ## script to perform the ML estimation of the complete exponetial model
 ## If the data are store in a file longlineData.csv,
 ## We may just use function Analysis with 
-## data the dataframe which stored the data to be analyzed
-## tune1 a parameter to tune due to the problem with numerical 
-##       minization, one may try value as 3,4, 5, 2
+## subdata the dataframe which stored the data to be analyzed
 ##############################################################################
 
-
-
-
-## function to minimize in order to obtain the MLestimator for the exponential model, 
-## with several longlines, with different soaktimes
-EquationNormaleLambda1 <- function(lambda2, Y)
-{
-  cn <- 1 + with(Y, sum(N1) / sum(N2) )
-  res <- with ( Y, - sum (P * Nb)  + 
-                           sum(  (N- Nb) * P * exp( - lambda2  * cn *P ) / 
-                                 ( 1 - exp( - lambda2  * cn *P ) ) 
-                               )
-              )
-    return( log(res^2+1) )
-}
-
-
-AnalysisMLE <- function(subdata, tune1=3) 
+AnalysisMLE <- function(subdata, tune1=3, sameP=TRUE, MEM=1) 
 {
   ## initial values for minimization algorithm
   if(!is.CLonglineData(subdata))
+  {
+    stop("Argument 1 of function AnalysisMLE is not of class CLOngloneData")
+  } else if( sum( subdata$N1+subdata$N2) == 0)
+  {
+    return(list( lambda = c( NA,NA, NA,NA ) ) ) ## no possible estimation
+  } else if(sameP)     
+  {
+    if(MEM==1) 
     {
-      stop("Argument 1 of function AnalysisMLE is not of class CLOngloneData")
+      return(Mem1MleSameP(subdata))
+    }else if(MEM==2)
+    {
+      return(Mem2MleSameP(subdata))
+    }             
+  } else
+  {
+    if(MEM==1) 
+    {
+      return(Mem1MleGeneral(subdata))
     } else
     {
-      if( sum( subdata$N1+subdata$N2 ) == 0)
-        {
-          return(list( lambda = c( NA,NA ) ) )
-        } else
-        {
-          
-          p <- mean(subdata$P)
-          algo.min.init <- with(subdata,sum(N1)/sum(N1+N2) * log( sum(N) /sum(Nb) )/p)
-          value.cur <- EquationNormaleLambda1(algo.min.init, subdata)
-          algo.min <-  nlminb(algo.min.init, EquationNormaleLambda1, lower = 0.0000001, upper = 0.01, Y=subdata)
-          iter=0
-          
-          ##print( paste(" Objective=", algo.min$objective, ", Par=", algo.min$par, sep="")) 
-          
-          while(iter < 10 & algo.min$objective< value.cur)
-            {
-              iter <- iter + 1
-              algo.min.init<- algo.min$par
-              value.cur <- algo.min$objective
-              algo.min <-  nlminb(algo.min.init, EquationNormaleLambda1, lower = algo.min.init - tune1 * algo.min.init / 10 , upper =algo.min.init +  tune1 * algo.min.init/10, Y=subdata) 
-            }
-          lambda2.est <- algo.min$par
-          lambda1.est <- with(subdata, sum(N1) / sum (N2) ) * lambda2.est
-          ##print(paste("lambda1.est=",lambda1.est,sep=""))
-          ##print(paste("lambda2.est=", lambda2.est,sep=""))
-          return(list(lambda = c(lambda1.est, lambda2.est)) )
-        }
+      return(Mem1MleGeneral(subdata))
     }
+  }
 }
 
 
-#################################################################
-##
-## Code for the log likelihood profile, for confidence interval
-## construction
-##################################################################
-
-MEMLoglike <- function(lambda2,lambda1,Y)
-  {
-    lambda <- lambda1 + lambda2
-    l <- with(Y,
-              sum(- lambda * Nb * P  +
-                  ( N1 + N2 + Ne) * log(1- exp(-lambda * P)) ) +
-              sum(N1) * log(lambda1) + sum(N2) * log(lambda2) -
-              sum(N1+N2) * log(lambda)
-              )
-    return(l)
-  }
-
-MEMLoglikeProfile <-  function(lambda1, Y)
-  {
-    op <- optimize(MEMLoglike, interval=c(0,2), maximum=T, lambda1=lambda1, Y=Y)
-    return(op$objective)
-  }
-
-MEMIC <- function(Y, lambda1.est)
-  {
-    epsilon <- 0.01
-    if(lambda1.est>0)
-      {
-        l.inf <- 1e-12
-        l.sup <- lambda1.est
-        l <- (l.inf+l.sup)/2
-        maxloglike <- MEMLoglikeProfile(lambda1.est, Y)
-        curloglike <- MEMLoglikeProfile(l, Y)
-        while( (abs(maxloglike - curloglike -1.35) > epsilon )  & ( abs(l.inf-l.sup) > 1e-10) )
-          {
-                                        #print(c(l.inf, l.sup) )
-            if( abs(maxloglike - curloglike) > (1.35 +epsilon) )
-              {
-                l.inf <- l
-                l <- (l.inf+l.sup)/2
-                curloglike <- MEMLoglikeProfile(l, Y)
-              }else
-            {
-              l.sup <- l
-              l <- (l.inf+l.sup)/2
-              curloglike <- MEMLoglikeProfile(l, Y)
-            }
-          }
-        l1 <- l
-      }else
+################################################
+## Estimators for MEM1 with shared soaktime P
+###############################################
+Mem1MleSameP <- function(subdata)
+{
+  with(subdata,
     {
-      l1 <- 0
+      hat.lambda1=sum( N1 ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      hat.lambda2=sum( N2 + Ne) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      hat.p2 = sum(Ne)/ sum(Ne+N2)
+      hat.p1 =0
+      return(c(hat.lambda1, hat.lambda2, hat.p1, hat.p2))
     }
+  )
+}
 
-    if(lambda1.est>0)
-      {
-        l.inf <- lambda1.est
-      }else
+################################################
+## Estimators for MEM2 with shared soaktime P
+###############################################
+Mem2MleSameP <- function(subdata)
+{
+  with(subdata,
     {
-      l.inf <- 1e-12
-      maxloglike <- MEMLoglikeProfile(l.inf, Y)
+      hat.lambda1=sum( N1 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      hat.lambda2=sum( N2 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      hat.p2 = sum( Ne )/ sum( N - Nb )
+      hat.p1 =  hat.p2 
+      return(c(hat.lambda1, hat.lambda2, hat.p1, hat.p2))
     }
-    l.sup <- 1.8
-    l <- (l.inf+l.sup)/2
-    curloglike <- MEMLoglikeProfile(l, Y)
-    while( (abs(maxloglike - curloglike -1.92) > epsilon ) & ( abs(l.inf-l.sup) > 1e-10) )
-      {
-        #print(c(l.inf, l.sup) )
-        if( abs(maxloglike - curloglike) > (1.92 +epsilon) )
-          {
-            l.sup <- l
-            l <- (l.inf+l.sup)/2
-            curloglike <- MEMLoglikeProfile(l, Y)
-          }else
-        {
-            l.inf <- l
-            l <- (l.inf+l.sup)/2
-            curloglike <- MEMLoglikeProfile(l, Y)
-        }
-      }
-    l2 <- l
-    return(c(l1,l2))
-  }
+  )
+}
+
+################################################
+## Estimators for MEM1 general case
+###############################################
+Mem1MleGeneral <- function(subdata)
+{
+  with(subdata,
+    {
+      lambda1.init = sum( N1 ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb)) 
+      lambda2.init = sum( N2 + Ne ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      p            = sum( Ne )/ sum( Ne + N2)
+      theta.mem1 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p) ) )
+      optim.mem1 = optim(theta.mem1, LogLike.Mem1, method="BFGS", control=list(fnscale=-1),  subdata=subdata)
+      par.mem1   = optim.mem1$par 
+      return( c(exp(par.mem1[1:2]), 0, exp(par.mem1[3]) / (1 + exp(par.mem1[3]) )) )
+    }
+  )
+}
+
+
+################################################
+## Estimators for MEM2 general case
+###############################################
+Mem2MleGeneral <- function(subdata)
+{
+  with(subdata,
+    {
+      lambda1.init = sum( N1 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb)) 
+      lambda2.init = sum( N2 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      p            = sum( Ne )/ sum( Ne + N2 + N1 )
+      theta.mem2 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
+      
+      optim.mem2 = optim(theta.mem2, LogLike.Mem2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      par.mem2   = optim.mem2$par 
+      return( c(par.mem2, par.mem2[3]) )
+    }
+  )
+}
+
+
+
+######################################################
+##   General LogLikelihood
+######################################################
+LogLike <- function(theta,  subdata)
+{
+  ## theta = c(log(lambda1), log(lambda2), logit(p1), logit(p2))
+  lambda1 = exp(theta[1])
+  lambda2 = exp(theta[2])
+  p1      = exp(theta[3]) / ( 1 + exp(theta[3]) )
+  p2      = exp(theta[4]) / ( 1 + exp(theta[4]) )
+  
+  with(subdata,
+    {
+      lambda=lambda1+lambda2
+      return (
+        sum( lgamma(N+1)-lgamma(Nb+1)-lgamma(N1+1)-lgamma(N2+1)-lgamma(Ne+1) ) - sum(lambda * P * Nb) +
+        sum( (N-Nb) * log (1 - exp( -lambda * P) ) ) + sum(N1 * log(  lambda1 / lambda * ( 1-p1) ) )+ 
+        sum( N2 * log(  lambda2 / lambda * ( 1-p2) ) ) + sum ( Ne * log(  (lambda1 *p1 + lambda2 * p2) / lambda ) )
+        )
+    }
+  )
+}
+
+######################################################
+##   MEM1 LogLikelihood
+######################################################
+LogLike.Mem1 <- function(theta.mem1,  subdata)
+{
+  ## theta.mem1 = c(log(lambda1), log(lambda2), logit(p2))
+  return( LogLike( c(theta.mem1[1:2], -Inf, theta.mem1[3]), subdata ) )
+}
+
+######################################################
+##   MEM2 LogLikelihood
+######################################################
+LogLike.Mem2 <- function(theta.mem2,  subdata)
+{
+  ## theta.mem1 = c(lambda1, lambda2, p)
+  return( LogLike( c(theta.mem2[1:2], theta.mem2[3], theta.mem2[3] ), subdata ) )
+}
+
+
+######################################################
+##   MEM1 AIC
+######################################################
+AIC.Mem1 <- function(subdata)
+{
+   with(subdata,
+    {
+      lambda1.init = sum( N1 ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb)) 
+      lambda2.init = sum( N2 ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      p            = sum( Ne )/ sum( Ne + N2 )
+      theta.mem1 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
+      
+      optim.mem1 = optim(theta.mem1, LogLike.Mem1, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      return( -2 * optim.mem2$value + 6)
+    }
+    )
+}
+
+######################################################
+##   MEM2 AIC
+######################################################
+AIC.Mem2 <- function(subdata)
+{
+   with(subdata,
+    {
+      lambda1.init = sum( N1 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb)) 
+      lambda2.init = sum( N2 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
+      p            = sum( Ne )/ sum( Ne + N2 + N1 )
+      theta.mem2 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
+      
+      optim.mem2 = optim(theta.mem2, LogLike.Mem2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      return( -2 * optim.mem2$value + 6)
+    }
+    )
+}
