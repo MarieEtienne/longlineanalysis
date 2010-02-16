b9d53feaccfea80177727986a8481b178de9da3e
MP.Etienne <MP.Etienne@3ae06b9c-c8ca-11de-9d4f-f947ee5921c8>
2010-02-16 19:29:11 +0000

    Simplification of the code :  Drawsamples is now implemented as in the paper

--- DrawSamples.R
+++ DrawSamples.R
@@ -1,63 +1,35 @@
 ### Draw samples according to the complete exponential model
-DrawSamples <- function(lambda1, lambda2, P, N, area=12, year=2003)
+DrawSamples <- function(lambda1, lambda2, P, N, p1=0, p2=0, area=12, year=2003)
 {
   I<- length(N)
-  
-  Y <- data.frame(AREA= NULL, Year=NULL, N0 = NULL, N1 = NULL, N2 = NULL, N = NULL, P = NULL)
-  for(i in 1:I){
-    T1 <- rexp(N[i], rate= lambda1 )
-    T2 <- rexp(N[i], rate= lambda2 )
-    T <- pmin(T1, T2)
-
-    ind.empty <- which(T>P[i])
-    ind.species1 <- which(T1<P[i] & T2>T1)
-    ind.species2 <- which(T2 < P[i] & T1 > T2)
-    N0 = sum(T > P[i])
-    N1 = sum(T1 < P[i] & T2 > T1)
-    N2 = sum(T2 < P[i] & T1 > T2)
-    Yrow <- data.frame(AREA=area, Year=year, N0 = N0, N1 = N1, N2 = N2, N = N[i], P = P[i])
-    Y<- rbind (Y, Yrow)
-  }
-
-  Y <- as.CLonglineData(Y$Year, Y$AREA, n=Y$N, n1= Y$N1, n2=Y$N2, nb=Y$N-Y$N1-Y$N2, ne=rep(0, I), p=Y$P)
-
+  lambda    = lambda1 + lambda2
+  Nb        = rbinom(I, N, prob=exp(- lambda * P) )
+  N1PlusNE1 = rbinom(I, N-Nb, prob=lambda1/lambda)
+  N2PlusNE2 = N-Nb-N1PlusNE1
+  N1        = rbinom(I, N1PlusNE1, prob=(1-p1))
+  NE1       = N1PlusNE1 - N1
+  N2        = rbinom(I, N2PlusNE2, prob=(1-p2))
+  NE2       = N2PlusNE2 - N2
+  NE        = NE1 + NE2
+  Y <- as.CLonglineData(year=rep(year,I), area=rep(area, I), n=N, n1= N1, n2=N2, nb=Nb, ne=NE, p=P)
  return(Y)
-
 }
 
 ### Draw samples according to the complete exponential and allow escapement
 DrawSampleswEscape <- function(par.simu)
 {
-  
-  sample.data <- with(par.simu,DrawSamples(lambda1, lambda2, rep(P, L) , rep(N, L), area=05, year=2020) )
-
   if(par.simu$type=="without")
   {
-    sample.data$Nempty <- rep(0, nrow(sample.data))
+    sample.data <- with(par.simu,DrawSamples(lambda1, lambda2, rep(P, L) , rep(N, L), area=05, year=2020) )
   }
   else if( par.simu$type=="uniform")
   {
-    n1.escape <- rbinom( nrow(sample.data), sample.data$N1, prob=par.simu$percent)
-    n2.escape <- rbinom( nrow(sample.data), sample.data$N2, prob=par.simu$percent)
-   sample.data$Nempty <- n1.escape + n2.escape
-    	
-    sample.data$N1 <- sample.data$N1 -n1.escape
-    sample.data$N2 <- sample.data$N2 - n2.escape
+    sample.data <- with(par.simu,DrawSamples(lambda1, lambda2, rep(P, L) , rep(N, L), p1=par.simu$percent, p2=par.simu$percent,  area=05, year=2020) )
   }
   else if (par.simu$type=="preferential")
   {
-    n1.escape <- rbinom( nrow(sample.data), sample.data$N1, prob=par.simu$percent)
-    n2.escape <- rbinom( nrow(sample.data), sample.data$N2, prob=par.simu$preffacteur*par.simu$percent)
-    sample.data$Nempty <- n1.escape + n2.escape
-    	
-    sample.data$N1 <- sample.data$N1 -n1.escape
-    sample.data$N2 <- sample.data$N2 - n2.escape
-   }
-  
-  sample.data <- with( sample.data,
-                      as.CLonglineData(YEAR, AREA, n=N, n1= N1, n2=N2, nb=N-N1-N2-Nempty, ne=Nempty, p=P)
-                      )
-##  print(sample.data)
+    sample.data <- with(par.simu,DrawSamples(lambda1, lambda2, rep(P, L) , rep(N, L), p1=par.simu$percent, p2=par.simu$preffacteur*par.simu$percent,  area=05, year=2020) )
+  }
   return(sample.data)
 }
 
@@ -65,36 +37,18 @@ DrawSampleswEscape <- function(par.simu)
 ### input is real design
 DrawSampleswEscape2 <- function(lambda1, lambda2, Y, type="without", percent=0.1, pref=1)
 {
-  
-  sample.data <- DrawSamples(lambda1, lambda2, Y$P , Y$N, area=05, year=2020) 
-
   if(type=="without")
   {
-    sample.data$Nempty <- rep(0, nrow(sample.data))
+   sample.data <- DrawSamples(lambda1, lambda2, Y$P , Y$N, area=05, year=2020) 
   }
   else if( type=="uniform")
   {
-    n1.escape <- rbinom( nrow(sample.data), sample.data$N1, prob=percent)
-    n2.escape <- rbinom( nrow(sample.data), sample.data$N2, prob=percent)
-   sample.data$Nempty <- n1.escape + n2.escape
-    	
-    sample.data$N1 <- sample.data$N1 -n1.escape
-    sample.data$N2 <- sample.data$N2 - n2.escape
+    sample.data <- DrawSamples(lambda1, lambda2, Y$P , Y$N, p1=percent, p2=percent, area=05, year=2020) 
   }
   else if (type=="preferential")
   {
-    n1.escape <- rbinom( nrow(sample.data), sample.data$N1, prob=percent)
-    n2.escape <- rbinom( nrow(sample.data), sample.data$N2, prob=pref*percent)
-    sample.data$Nempty <- n1.escape + n2.escape
-    	
-    sample.data$N1 <- sample.data$N1 -n1.escape
-    sample.data$N2 <- sample.data$N2 - n2.escape
+    sample.data <- DrawSamples(lambda1, lambda2, Y$P , Y$N, p1=percent, p2=pref*percent, area=05, year=2020) 
    }
-  
-  sample.data <- with( sample.data,
-                      as.CLonglineData(YEAR, AREA, n=N, n1= N1, n2=N2, nb=N-N1-N2-Nempty, ne=Nempty, p=P)
-                      )
-##  print(sample.data)
   return(sample.data)
 }
 
