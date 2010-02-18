78c523f8c5d454795ce6f4f54ed7c5f326d52950
MP.Etienne <MP.Etienne@3ae06b9c-c8ca-11de-9d4f-f947ee5921c8>
2010-02-18 01:40:11 +0000

    ADDED : MEM.Cov function to compute the asymptotic covariance matrix ADDED : Prediction.SEM function to compute expected values  for SEM ADDED : Prediction.MEM function to compute expected values of MEM

--- MEM.R
+++ MEM.R
@@ -81,7 +81,7 @@ Mem1MleGeneral <- function(subdata)
       lambda2.init = sum( N2 + Ne ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
       p            = sum( Ne )/ sum( Ne + N2)
       theta.mem1 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p) ) )
-      optim.mem1 = optim(theta.mem1, LogLike.Mem1, method="BFGS", control=list(fnscale=-1),  subdata=subdata)
+      optim.mem1 = optim(theta.mem1, LogLike.MEM1, method="BFGS", control=list(fnscale=-1),  subdata=subdata)
       par.mem1   = optim.mem1$par 
       return( c(exp(par.mem1[1:2]), 0, exp(par.mem1[3]) / (1 + exp(par.mem1[3]) )) )
     }
@@ -101,7 +101,7 @@ Mem2MleGeneral <- function(subdata)
       p            = sum( Ne )/ sum( Ne + N2 + N1 )
       theta.mem2 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
       
-      optim.mem2 = optim(theta.mem2, LogLike.Mem2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      optim.mem2 = optim(theta.mem2, LogLike.MEM2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
       par.mem2   = optim.mem2$par 
       return( c(par.mem2, par.mem2[3]) )
     }
@@ -135,23 +135,20 @@ LogLike <- function(theta,  subdata)
 }
 
 ######################################################
-##   MEM1 LogLikelihood
+##   MEM LogLikelihood
 ######################################################
-LogLike.Mem1 <- function(theta.mem1,  subdata)
+LogLike.MEM1 <- function(theta.mem1,  subdata)
 {
-
   ## theta.mem1 = c(log(lambda1), log(lambda2), logit(p2))
   return( LogLike( c(theta.mem1[1:2], -Inf, theta.mem1[3]), subdata=subdata ) )
 }
 
-######################################################
-##   MEM2 LogLikelihood
-######################################################
-LogLike.Mem2 <- function(theta.mem2,  subdata)
+LogLike.MEM2 <- function(theta.mem2,  subdata)
 {
-  ## theta.mem1 = c(lambda1, lambda2, p)
+  ## theta.mem2 = c(log(lambda1), log(lambda2), logit(p))
   return( LogLike( c(theta.mem2[1:2], theta.mem2[3], theta.mem2[3] ), subdata=subdata ) )
 }
+
 
 
 ######################################################
@@ -165,7 +162,7 @@ AIC.Mem1 <- function(subdata)
       lambda2.init = sum( N2 ) / ( mean(P) * sum( (N - Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
       p            = sum( Ne )/ sum( Ne + N2 )
       theta.mem1 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
-      optim.mem1 = optim(theta.mem1, LogLike.Mem1, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      optim.mem1 = optim(theta.mem1, LogLike.MEM1, method="BFGS", control=list(fnscale=-1), subdata=subdata)
       return( -2 * optim.mem1$value + 6)
     }
     )
@@ -183,7 +180,7 @@ AIC.Mem2 <- function(subdata)
       p            = sum( Ne )/ sum( Ne + N2 + N1 )
       theta.mem2 = c(log(lambda1.init), log(lambda2.init), log(p/(1-p)))
       
-      optim.mem2 = optim(theta.mem2, LogLike.Mem2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
+      optim.mem2 = optim(theta.mem2, LogLike.MEM2, method="BFGS", control=list(fnscale=-1), subdata=subdata)
       return( -2 * optim.mem2$value + 6)
     }
     )
@@ -193,39 +190,116 @@ AIC.Mem2 <- function(subdata)
 ######################################################
 ##   Profile likelihood for lambda1 in MEM1
 ######################################################
-Profile.Mem1 <- function(lambda1, subdata)
+Profile.MEM <- function(lambda1, subdata, MEM=1)
 {
-       with(subdata,
-      {
-        lambda2.init = sum( N2 + Ne) / ( mean(P) * sum( (N- Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) since all p are supposed to be equal or can be considered as equal
-        p2           = sum( Ne )/ sum( Ne + N2  )
-        theta.mem1 = c(log(lambda2.init), log(p2/(1-p2)))
-        optim.mem1 = optim(theta.mem1 , Partial.Mem1, method="BFGS", control=list(fnscale=-1), lambda1=lambda1, subdata=subdata)
-        return( optim.mem1$value )
+  ## lambda 1 is a vector 
+  with(subdata,
+       {
+         return( sapply(lambda1, PartialLogLike, subdata=subdata, MEM=MEM))
       }
   )
 }
 ######################################################
 ##   Partial likelihood for lambda1 in MEM1
+##  optimized according other parameters
 ######################################################
-Partial.Mem1 <- function(theta, lambda1, subdata)
+PartialLogLike.MEM<- function(lambda1, subdata, MEM=1)
 {
-   with(subdata,
-    {
-      return(LogLike.Mem1(c(log(lambda1), theta)), subdata=subdata)
-    }
-    )
+  with(subdata,
+       {
+         if(MEM==1)
+           {
+             lambda2.init = sum( N2 + Ne) / ( mean(P) * sum( (N- Nb) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) : initial guess
+             p2           = sum( Ne )/ sum( Ne + N2 )
+             theta.mem1 = c( log(lambda2.init), log(p2/(1-p2)))
+             ## definition of the function to be optimized only on lambda2 and p2
+             partlog <- function(theta.mem1)
+               {
+                 return (LogLike.MEM1(c(log(lambda1), theta.mem1), subdata=subdata) )
+               }
+             optim.mem1 = optim(theta.mem1 , partlog, method="SANN", control=list(fnscale=-1))
+             return( optim.mem1$value )
+           }else if(MEM==2)
+           {
+             lambda2.init = sum( N2 ) / ( mean(P) * sum( (N1 + N2) ) ) * log( sum(N) / sum(Nb))  # P=mean(P) : initial guess
+             p            = sum( Ne )/ sum( Ne + N2  + N1)
+             theta.mem2   = c( log(lambda2.init), log(p/(1-p)))
+             ## definition of the function to be optimized only on lambda2 and p2
+             partlog <- function(theta.mem2)
+               {
+                 return (LogLike.MEM2(c(log(lambda1), theta.mem2), subdata=subdata) )
+               }
+             optim.mem2 = optim(theta.mem2 , partlog, method="BFGS", control=list(fnscale=-1))
+             return( optim.mem2$value )
+           }
+       }
+  )
 }
 
 
-######################################################
-##   Partial likelihood for lambda1 in MEM2
-######################################################
-Profile.Mem2 <- function(theta, lambda1, subdata)
+#################################################
+##Prediction of expected values
+##
+#################################################
+Prediction.MEM <-  function(theta, subdata)
 {
-   with(subdata,
-    {
-      return(LogLike.Mem2(c(log(lambda1), theta)), subdata=subdata)
-    }
-    )
-}
+  lambda1=theta[1]
+  lambda2=theta[2]
+  lambda=lambda1+lambda2
+  p1=theta[3]
+  p2=theta[4]
+  with(subdata,
+       {
+         N1.pred= N*(1-exp(-lambda * P)) * lambda1 /lambda *(1-p1)
+         N2.pred= N*(1-exp(-lambda * P)) * lambda2 /lambda *(1-p2)
+         Ne.pred= N*(1-exp(-lambda * P)) * (lambda1*p1 +lambda2*p2) /lambda
+         return(list(N1.pred=N1.pred, N2.pred=N2.pred, Ne.pred=Ne.pred))
+       }
+       )
+  }
+
+
+################################################
+## Covariance matrix asymptotic
+## only valid if all P are similar
+###############################################
+
+MEM.Cov <- function(subdata, MEM=1)
+  {
+    P=mean(subdata$P)
+    theta=MEM.MLE(subdata, MEM=MEM)
+    lambda1   = theta[1]
+    lambda2   = theta[2]
+    p1        = theta[3]
+    p2        = theta[4]
+    lambda    = lambda1+lambda2
+    moinsexp = 1-exp(-lambda*P)
+
+    if(MEM==1)
+      {
+        mat= matrix(c(lambda1*lambda2 / moinsexp + moinsexp *lambda1^2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          -lambda1*lambda2 / moinsexp + moinsexp *lambda1*lambda2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          0,
+          -lambda1*lambda2 / moinsexp + moinsexp *lambda1*lambda2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          lambda1*lambda2 / moinsexp + moinsexp *lambda2^2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          0,
+          0,
+          0,
+          lambda*p2*(1-p2)/ (lambda2 * moinsexp)), byrow=T, ncol=3)
+      }else if( MEM==2)
+      {
+      mat= matrix(c(lambda1*lambda2 / ((1-p2)* moinsexp) + moinsexp *lambda1^2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          -lambda1*lambda2 / ((1-p2)* moinsexp) + moinsexp *lambda1*lambda2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          0,
+          -lambda1*lambda2 / ((1-p2)* moinsexp) + moinsexp *lambda1*lambda2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          lambda1*lambda2 / ((1-p2)* moinsexp) + moinsexp *lambda2^2 / ( P^2 * exp(-lambda *P) * lambda^2),
+          0,
+          0,
+          0,
+          p2*(1-p2)/ (moinsexp)), byrow=T, ncol=3)
+        
+      }
+    
+    
+    return(1/sum(subdata$N) * mat)
+  }
--- SEM.R
+++ SEM.R
@@ -62,7 +62,7 @@ Sem2MleGeneral <- function(subdata, verbose=F)
                     (N2 - lambda2.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 +
                     (Ne - lambdae.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 ) / ( 3 * length(N) ) )
       theta.sem2 = c( log(lambda1.init),log(lambda2.init),log(lambdae.init),log(sigma.init))
-      sem2 = optim(theta.sem2,  SEM2Loglike, method="BFGS", control=list(fnscale=-1),subdata=subdata)
+      sem2 = optim(theta.sem2,  LogLike.SEM2, method="BFGS", control=list(fnscale=-1),subdata=subdata)
       if(verbose) print(sem2)
       hat.sem2 = exp(sem2$par)
       return(c(hat.sem2) )
@@ -74,8 +74,7 @@ Sem2MleGeneral <- function(subdata, verbose=F)
 ##############################################
 ##    log likelihood for SEM1
 ##############################################
-SEM1Loglike <- function(theta.sem1,  subdata) 
->>>>>>> .r10
+LogLike.SEM1 <- function(theta.sem1,  subdata) 
 {
   ##theta.sem1=c(log(lambda1), log(lambda2), log(sigma))
   lambda1 = exp(theta.sem1[1])
@@ -93,7 +92,7 @@ SEM1Loglike <- function(theta.sem1,  subdata)
 ##############################################
 ##    log likelihood for SEM2
 ##############################################
-SEM2Loglike <- function(theta.sem2,  subdata) 
+LogLike.SEM2 <- function(theta.sem2,  subdata) 
 {
   ##theta.sem1=c(log(lambda1), log(lambda2), log(lambdae), log(sigma))
   lambda1 = exp(theta.sem2[1])
@@ -103,9 +102,11 @@ SEM2Loglike <- function(theta.sem2,  subdata)
   lambda  = lambda1 + lambda2 + lambdae
   with(subdata,
      {
-       return( sum( dnorm(N1, mean=lambda1/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) )+
-      sum( dnorm(N2, mean=lambda2/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) )   +
-      sum( dnorm(Ne, mean=lambdae/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) ))
+       return(
+       sum( dnorm(N1, mean=lambda1/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) ) +
+       sum( dnorm(N2, mean=lambda2/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) ) +
+       sum( dnorm(Ne, mean=lambdae/lambda*N * (1-exp(-lambda * P)), sd=sigma, log=T) )
+              )
      }
   )
 }
@@ -123,7 +124,7 @@ AIC.Sem1 <- function(subdata)
       sigma.init   = sqrt ( sum( (N1 - lambda1.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 + 
                     (N2+Ne - lambda2.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 ) / ( 2 * length(N) ) )
       theta.sem1 = c( log(lambda1.init),log(lambda2.init),log(sigma.init))
-      sem1 = optim(theta.sem1,  SEM1Loglike, method="BFGS", control=list(fnscale=-1),subdata=subdata)
+      sem1 = optim(theta.sem1,  LogLike.SEM1, method="BFGS", control=list(fnscale=-1),subdata=subdata)
       return(-2*sem1$value+6)
     }
   )
@@ -144,11 +145,108 @@ AIC.Sem2 <- function(subdata)
                     (N2 - lambda2.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 +
                     (Ne - lambdae.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 ) / ( 3 * length(N) ) )
       theta.sem2 = c( log(lambda1.init),log(lambda2.init),log(lambdae.init),log(sigma.init))
-      sem2 = optim(theta.sem2,  SEM2Loglike, method="BFGS", control=list(fnscale=-1),subdata=subdata)
+      sem2 = optim(theta.sem2,  LogLike.SEM2, method="BFGS", control=list(fnscale=-1),subdata=subdata)
       return(-2*sem2$value + 8)
->>>>>>> .r10
     }
   )
 }
   
+
+######################################################
+##   Profile likelihood for lambda1 in MEM1
+######################################################
+Profile.SEM <- function(lambda1, subdata, SEM=1)
+{
+  ## lambda 1 is a vector 
+  with(subdata,
+       {
+         return( sapply(lambda1, PartialLogLike.SEM, subdata=subdata, SEM=SEM))
+      }
+  )
+}
+######################################################
+##   Partial likelihood for lambda1 in MEM1
+##  optimized according other parameters
+######################################################
+PartialLogLike.SEM<- function(lambda1, subdata, SEM=1)
+{
+  with(subdata,
+       {
+         ##################
+         ##CAS SEM 1
+         if(SEM==1)
+           {
+             
+             lambda.init  = 1/ mean(P)  * log( sum(N) / sum(Nb))  # P=mean(P) : initial guess
+             if(lambda1<lambda.init)
+               {
+                 lambda2.init = lambda.init - lambda1
+               } else
+             {
+               lambda2.init=lambda1
+               lambda.init=lambda2.init + lambda1  
+             }
+             sigma.init   = sqrt ( sum( (N1 - lambda1/lambda.init*N *(1-exp(-lambda.init*P)))^2 + 
+               (N2+Ne - lambda2.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 ) / ( 2 * length(N) ) )
+             theta.sem1 = c( log(lambda2.init), log(sigma.init))
+             ## definition of the function to be optimized only on lambda2 and p2
+             partlog <- function(theta.sem1)
+               {
+                 return (LogLike.SEM1(c(log(lambda1), theta.sem1), subdata=subdata) )
+               }
+             optim.sem1 = optim(theta.sem1 , partlog, method="BFGS", control=list(fnscale=-1))
+             return( optim.sem1$value )
+           }else if(SEM==2)
+             ####################################
+             ## CAS SEM2
+           {
+
+             ## finding initial values lambda2, lambdae and sigma
+             ## for optim
+             ## if lambda1 
+             lambda.init  = 1/ mean(P)  * log( sum(N) / sum(Nb))  # P=mean(P) : initial guess
+             if(lambda1<lambda.init)
+               {
+                 lambda2.init = sum(N2) /sum(N1+N2) *  ( lambda.init - lambda1)
+                 lambdae.init = max(lambda.init - lambda1 -lambda2.init,0)
+               } else
+             {
+               lambda2.init=lambda1/2
+               lambdae.init=lambda1/2
+               lambda.init=lambdae.init+lambda2.init+ lambda1
+             }
+             sigma.init   = sqrt (
+               sum( (N1 - lambda1/lambda.init*N *(1-exp(-lambda.init*P)))^2 + 
+               (N2 - lambda2.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 +
+               (Ne - lambdae.init/lambda.init*N *(1-exp(-lambda.init*P)))^2 ) / ( 3 * length(N) ) )
+             theta.sem2 = c( log(lambda2.init),log(lambdae.init), log(sigma.init))
+             ## definition of the function to be optimized only on lambda2 and p2
+             partlog <- function(theta.sem2)
+               {
+                 return (LogLike.SEM2(c(log(lambda1), theta.sem2), subdata=subdata) )
+               }
+             optim.sem2 = optim(theta.sem2 , partlog, method="BFGS", control=list(fnscale=-1))
+             return( list(value=optim.sem2$value, lambda2=optim.sem2$par[2],lambdae=optim.sem2$par[2], sig=optim.sem2$par[3]))
+           }
+       }
+  )
+}
+
+
+Prediction.SEM <-  function(theta, subdata)
+{
+  lambda1=theta[1]
+  lambda2=theta[2]
+  lambdae=theta[3]
+  sigma=theta[4]
+  lambda=lambda1+lambda2+lambdae
+  with(subdata,
+       {
+         N1.pred= N*(1-exp(-lambda * P)) * lambda1 /lambda 
+         N2.pred= N*(1-exp(-lambda * P)) * lambda2 /lambda
+         Ne.pred= N*(1-exp(-lambda * P)) * lambdae /lambda
+         return(list(N1.pred=N1.pred, N2.pred=N2.pred, Ne.pred=Ne.pred))
+       }
+       )
+  }
 
